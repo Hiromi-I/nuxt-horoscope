9d9573087d67e98f3a87706557dcffc8da1ee8ca
hiro <hiromi.iwai.04.03@gmail.com>
2023-08-31 14:19:43 +0900

    初期画面のコンポーネントを実装

--- components/InitialGuide.vue
+++ components/InitialGuide.vue
@@ -1,3 +1,39 @@
 <template>
-    <p>日付を選択してください。</p>
+  <div class="container">
+    <p class="info">日付を選択して下さい</p>
+    <picture>
+      <source
+        srcset="@/assets/images/signs-lg.png"
+        media="(min-width: 1200px)"
+      />
+      <source
+        srcset="@/assets/images/signs-md.png"
+        media="(min-width: 768px)"
+      />
+      <img
+        src="@/assets/images/signs-sm.png"
+        alt="星座一覧"
+        class="image"
+      />
+    </picture>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  background-color: white;
+  border: solid 2px var(--sub-color);
+  padding: 40px;
+  margin: 40px 0 0;
+}
+.info {
+  color: var(--theme-color);
+  font-size: 36px;
+  text-align: center;
+  margin-bottom: 30px;
+}
+.image {
+  width: 100%;
+  height: auto;
+}
+</style>
