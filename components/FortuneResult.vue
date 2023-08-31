672499d2595aa76c59abe1b4bedb4d4cce15a2bf
hiro <hiromi.iwai.04.03@gmail.com>
2023-08-31 10:10:58 +0900

    占い結果を星座毎に表示

--- components/FortuneResult.vue
+++ components/FortuneResult.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type { API_Response } from "@/types/horoscope";
+
 const props = defineProps<{
   targetDate: string,
 }>()
@@ -9,18 +11,39 @@ url.searchParams.set('year', year)
 url.searchParams.set('month', month)
 url.searchParams.set('day', day)
 
-const { data, pending, error } = await useAsyncData(
+const { data, pending, error } = await useAsyncData<API_Response>(
   url.href,
-  () => $fetch(url.href)
+  () => $fetch<API_Response>(url.href)
 )
+const results = data.value?.horoscope[`${year}/${month}/${day}`]
 </script>
 
 <template>
   <div>
     <p v-if="pending">Loading...</p>
     <p v-else-if="error">Error...</p>
-    <pre v-else>
-      {{ data }}
-    </pre>
+    <ul v-else class="signsList">
+      <SignItem
+        v-for="item in results"
+        :key="`${year}-${month}-${day}-item.sign`"
+        :result="item"
+      />
+    </ul>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.signsList {
+  display: grid;
+  gap: 20px;
+  grid-template-columns: repeat(1, 1fr);
+
+  @include tab {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @include pc {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+</style>
