b2cb5867e9e9b5c783fb1307afbba7fb4349ea45
hiro <hiromi.iwai.04.03@gmail.com>
2023-08-31 10:06:43 +0900

    運の星表示用のコンポーネントを作成

---
+++ components/LuckyStars.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  score: number,
+}>()
+</script>
+
+<template>
+  <span>
+    <span v-for="count in score" :key="`lucky-${count}`">&starf;</span>
+    <span v-for="count in 5 - score" :key="`unlucky-${count}`">&star;</span>
+  </span>
+</template>
