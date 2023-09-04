77dcfe8a43b3bb51dc67b241f90768efaeef77e7
hiro <hiromi.iwai.04.03@gmail.com>
2023-09-04 22:33:23 +0900

    テスト用の設定を記述

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+import Vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    Vue(),
+  ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname),
+      '@': path.resolve(__dirname)
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.ts']
+  }
+})
