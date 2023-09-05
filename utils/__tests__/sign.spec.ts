fcf5610b76a999f4bdbeebb837f5e5115b161f79
hiro <hiromi.iwai.04.03@gmail.com>
2023-09-05 20:31:01 +0900

    getImagePathのテストを作成

---
+++ utils/__tests__/sign.spec.ts
@@ -0,0 +1,21 @@
+import { describe, test, expect } from 'vitest'
+import { getImagePath } from '@/utils/sign'
+
+
+describe('sign', () => {
+  test('aries', () => {
+    const jSignName = '牡羊座'
+
+    const imagePath = getImagePath(jSignName)
+
+    expect(imagePath).toMatch(/\/assets\/images\/aries.jpg$/)
+  })
+
+  test('aries', () => {
+    const jSignName = '牡牛座'
+
+    const imagePath = getImagePath(jSignName)
+
+    expect(imagePath).toMatch(/\/assets\/images\/taurus.jpg$/)
+  })
+})
